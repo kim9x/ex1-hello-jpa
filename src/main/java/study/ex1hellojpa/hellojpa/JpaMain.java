f64a0f68b1f15c5a6ca91660cbef66233fd353f8
Kim9xd <kim9xd@gmail.com>
2021-12-08 16:53:04 +0900

    * 프록시와 연관관계 관리   - 영속성 전이(CASCADE)와 고아 객체

--- src/main/java/study/ex1hellojpa/hellojpa/JpaMain.java
+++ src/main/java/study/ex1hellojpa/hellojpa/JpaMain.java
@@ -22,29 +22,24 @@ public class JpaMain {
 		tx.begin();
 		
 		try {
-			Team team = new Team();
-			team.setName("teamA");
-			em.persist(team);
+			Child child1 = new Child();
+			Child child2 = new Child();
 			
-			Member member1 = new Member();
-			member1.setUsername("member1");
-			member1.setTeam(team);
-			em.persist(member1);  
+			Parent parent = new Parent();
+			parent.addChild(child1);
+			parent.addChild(child2);
+			
+			em.persist(parent);
+			
+			// cascade로 관리
+//			em.persist(child1);
+//			em.persist(child2);
 			
 			em.flush();
 			em.clear();
 			
-//			Member m = em.find(Member.class, member1.getId());
-			
-//			System.out.println("m = " + m.getTeam().getClass());
-			
-//			System.out.println("====================");
-//			m.getTeam().getName(); // 초기화
-//			System.out.println("====================");
-			
-			// fetch 조인 사용 시 필요한 경우 쿼리 한번으로 관계된 데이터를 모두 가져옴
-			List<Member> members = em.createQuery("select m from Member m join fetch m.team"  , Member.class)
-											.getResultList();
+			Parent findParent = em.find(Parent.class, parent.getId());
+			findParent.getChildList().remove(0);
 			
 			tx.commit();
 			
@@ -56,12 +51,6 @@ public class JpaMain {
  		emf.close();
 		
 		
-	}
-
-	private static void logic(Member m1, Member m2) {
-//		System.out.println("m1 == m2: " + (m1.getClass() == m2.getClass()));
-		System.out.println("m1 == Member: " + (m1 instanceof Member));
-		System.out.println("m2 == Member: " + (m2 instanceof Member));
 	}
 
 }
